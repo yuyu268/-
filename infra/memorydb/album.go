6dd6fad6b3bfe3b159ed68df4071fcded1a74bd9
yuyu268 <118980131+yuyu268@users.noreply.github.com>
2023-01-05 01:30:16 +0900

    Update album.go

--- infra/memorydb/album.go
+++ infra/memorydb/album.go
@@ -22,3 +22,39 @@ func NewAlbumRepository() *albumRepository {
 		albumMap: initMap,
 	}
 }
+
+func (r *albumRepository) GetAll(ctx context.Context) ([]*model.Album, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	albums := make([]*model.Album, 0, len(r.albumMap))
+	for _, s := range r.albumMap {
+		albums = append(albums, s)
+	}
+	return albums, nil
+}
+
+func (r *albumRepository) Get(ctx context.Context, id model.AlbumID) (*model.Album, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	album, ok := r.albumMap[id]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+	return album, nil
+}
+
+func (r *albumRepository) Add(ctx context.Context, album *model.Album) error {
+	r.Lock()
+	r.albumMap[album.ID] = album
+	r.Unlock()
+	return nil
+}
+
+func (r *albumRepository) Delete(ctx context.Context, id model.AlbumID) error {
+	r.Lock()
+	delete(r.albumMap, id)
+	r.Unlock()
+	return nil
+}
