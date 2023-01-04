944ae60d37ab75656d015d133df354005e7f0174
yuyu268 <118980131+yuyu268@users.noreply.github.com>
2023-01-04 18:39:07 +0900

    Update album.go

--- repository/album.go
+++ repository/album.go
@@ -9,6 +9,6 @@ import (
 type AlbumRepository interface {
 	GetAll(ctx context.Context) ([]*model.Album, error)
 	Get(ctx context.Context, id model.AlbumID) (*model.Album, error)
-	Add(ctx context.Context, album *model.Album) error
+	Add(ctx context.Context, singer *model.Album) error
 	Delete(ctx context.Context, id model.AlbumID) error
 }
